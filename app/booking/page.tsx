2e99bae8ba08d6283c5db95e56076167aad02d09
Melbin Mathew <melbinppmathewp@gmail.com>
2026-01-23 23:07:37 +0530

    10th commit - Added mails for payment and booking 6

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,16 +1,10 @@
 import BookingPage from "@/components/user/booking/Main";
+import api from "../lib/axios";
 
 async function getPrices() {
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/services/prices`,
-        { cache: 'no-store' }
-    );
+    const res = await api.get("/services/prices");
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch prices');
-    }
-
-    return res.json();
+    return res.data;
 }
 
 export default async function Booking() {
