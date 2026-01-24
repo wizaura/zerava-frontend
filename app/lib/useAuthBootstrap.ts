ed34720f43fe133070ea4e5d2d1b9a06dc91c78f
Melbin Mathew <melbinppmathewp@gmail.com>
2026-01-24 11:27:42 +0530

    10th commit - Added mails for payment and booking 7.2

--- app/lib/useAuthBootstrap.ts
+++ app/lib/useAuthBootstrap.ts
@@ -1,26 +1,32 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import api from "./axios";
 import {
     setUser,
     clearAuth,
     setBootstrapped,
 } from "@/app/store/slices/authSlice";
+import { fetchMeGracefully } from "./fetchGrace";
+import { usePathname } from "next/navigation";
 
 export const useAuthBootstrap = () => {
     const dispatch = useDispatch();
+    const pathname = usePathname();
 
     useEffect(() => {
+        if (pathname.startsWith("/admin")) {
+            dispatch(setBootstrapped());
+            return;
+        }
+
         const initAuth = async () => {
             try {
-                const res = await api.get("/auth/me");
+                const res = await fetchMeGracefully();
                 dispatch(setUser(res.data.user));
             } catch {
-                // not logged in is OK
                 dispatch(setBootstrapped());
             }
         };
 
         initAuth();
-    }, [dispatch]);
+    }, [dispatch, pathname]);
 };
